6a39655058b2d9ab76520b0fc392a1cdee6adbb8
ChamkhiAnas <mahmoudbennouna@gmail.com>
2020-04-09 11:55:49 +0100

    create method

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+@section('content')
+
+<form action="">
+    <div><label for=""></label><input type="text"></div>
+    <div><label for=""></label><input type="text"></div>
+</form>
+
+@endsection
