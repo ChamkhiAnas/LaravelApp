d6ad6a1c14b3a09148e79f43c44de1a50d640739
ChamkhiAnas <mahmoudbennouna@gmail.com>
2020-04-12 12:38:30 +0100

    creatingTestPostPage

---
+++ tests/Feature/PostTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PostTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testSavePost()
+    {
+
+    }
+}
